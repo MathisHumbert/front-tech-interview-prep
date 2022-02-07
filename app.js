4786f38332db8fbae9c1e293354b7c03e15dda3d
MathisHumbert <84037244+MathisHumbert@users.noreply.github.com>
2022-02-07 15:54:57 +0100

    working on tec interview

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // function that takes two arguments and return the sum
 const add = (num1, num2) => num1 + num2;
-console.log(add(3, 4));
+// console.log(add(3, 4));
 
 // function that takes any arguments and return the sum
 function sum(...args) {
@@ -8,16 +8,16 @@ function sum(...args) {
     return acc + curr;
   }, 0);
 }
-console.log(sum(2, 3, 4, 5));
+// console.log(sum(2, 3, 4, 5));
 
 // function that takes two strings and return if the first include the second arguement
 const stringIncludes = (sentence, word) => {
   const sentenceArr = sentence.toLowerCase().split(' ');
   return sentenceArr.includes(word.toLowerCase());
 };
-console.log(
-  stringIncludes('I drove to new York in a van with my friend', 'new')
-);
+// console.log(
+//   stringIncludes('I drove to new York in a van with my friend', 'new')
+// );
 
 // function that return an array of the object "name" key
 const getNames = (arr) => {
@@ -28,16 +28,16 @@ const getNames = (arr) => {
     return acc;
   }, []);
 };
-console.log(
-  getNames([
-    { a: 1 },
-    { name: 'Jane' },
-    {},
-    { name: 'Mark' },
-    { name: 'Sophia' },
-    { b: 2 },
-  ])
-);
+// console.log(
+//   getNames([
+//     { a: 1 },
+//     { name: 'Jane' },
+//     {},
+//     { name: 'Mark' },
+//     { name: 'Sophia' },
+//     { b: 2 },
+//   ])
+// );
 
 // function that takes an array of numbers and returns the index of the largest number
 const getLargestNumberIndex = (arr) => {
@@ -48,16 +48,68 @@ const getLargestNumberIndex = (arr) => {
     })
   );
 };
-console.log(getLargestNumberIndex([7, 1, 4, 12, 9]));
+// console.log(getLargestNumberIndex([7, 1, 4, 12, 9]));
 
+// write a function that returns a promise
 const delay = (n) => {
   return new Promise((resolve, reject) => setTimeout(() => resolve(), n));
 };
+// (async () => {
+//   console.log('Testing delay');
+//   await delay(1000);
+//   console.log('Testing delay end');
+// })();
 
-(async () => {
-  console.log('Testing delay');
-  await delay(1000);
-  console.log('Testing delay end');
-})();
+// ARRAY
+let myArray = ['a', 'b', 'c', 'd'];
 
-// write a function that returns a promise
+// doesn't create a new array, just modify the existing one
+// myArray.push('end');
+// myArray.unshift('start');
+
+// ES6
+myArray = ['start', ...myArray];
+myArray = [...myArray, 'end'];
+// myArray = ['start', ...myArray, 'end'];
+console.log(myArray);
+
+// Create a private variable
+function secretVariable() {
+  let private = 'super secret code';
+  return function () {
+    return private;
+  };
+}
+let getPrivateVariable = secretVariable();
+console.log(getPrivateVariable());
+
+// What is the output ?
+var num = 4;
+function outer() {
+  var num = 2;
+  function inner() {
+    num++;
+    var num = 3;
+    console.log(num);
+  }
+  inner();
+}
+outer();
+// 3
+
+// What is the output ?
+console.log(typeof typeof 1);
+// ==> typeof('number')
+// string
+
+// What is the output ?
+let hero = {
+  _name: 'John Doe',
+  getSecretIdentity() {
+    return this._name;
+  },
+};
+let stoleSecretIdentity = hero.getSecretIdentity;
+// let stoleSecretIdentity = hero.getSecretIdentity.bind(hero); will work
+console.log(stoleSecretIdentity());
+console.log(hero.getSecretIdentity());
